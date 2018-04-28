71d6b53ba09de89716ae8cd5717b0dc56c0a536c
Paul Malone <pmalone13@gmail.com>
2018-04-28 15:45:41 -0400

    changed cookie name

--- src/main/java/com/elliott/stash/servlet/MyServlet.java
+++ src/main/java/com/elliott/stash/servlet/MyServlet.java
@@ -12,7 +12,7 @@ public class MyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-		Cookie cookie = new Cookie("JSESSIONID", "");
+		Cookie cookie = new Cookie("BITBUCKETSESSIONID", "");
 		cookie.setMaxAge(0);
 		cookie.setPath("/");
         resp.addCookie(cookie);
